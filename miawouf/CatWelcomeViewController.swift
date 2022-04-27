c99c8ecf935db94af828eef3d4b0b1868ef6b847
Sebastien <delorme38@hotmail.com>
2022-04-27 17:27:56 -0400

    Fin du chapitre 5

---
+++ miawouf/CatWelcomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CatWelcomeViewController.swift
+//  miawouf
+//
+//  Created by Sébastien Delorme (Étudiant) on 2022-04-27.
+//
+
+import UIKit
+
+class CatWelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
