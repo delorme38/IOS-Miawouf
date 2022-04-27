bdc77e83fb3274a3586b084953c474053e4328bf
Sebastien <delorme38@hotmail.com>
2022-04-27 13:38:27 -0400

    Initial Commit

---
+++ miawouf/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  miawouf
+//
+//  Created by Sébastien Delorme (Étudiant) on 2022-04-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
