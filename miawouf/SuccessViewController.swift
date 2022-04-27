49ac4ed9c44a71cfa0597b860d44eeb5ec756d65
Sebastien <delorme38@hotmail.com>
2022-04-27 16:59:37 -0400

    initial commit

---
+++ miawouf/SuccessViewController.swift
@@ -0,0 +1,32 @@
+//
+//  SuccessViewController.swift
+//  miawouf
+//
+//  Created by Sébastien Delorme (Étudiant) on 2022-04-27.
+//
+
+import UIKit
+
+class SuccessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func dismiss(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ miawouf/WelcomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WelcomeViewController.swift
+//  miawouf
+//
+//  Created by Sébastien Delorme (Étudiant) on 2022-04-27.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func unwindToWelcome(segue:UIStoryboardSegue) { }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
